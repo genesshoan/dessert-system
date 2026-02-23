dd6dfb52ed770267be57ff10ad47cc73e9c02413
Shoan Genes <shoangenes42@gmail.com>
2026-02-23 16:05:28 -0300

    Add AppDataPersistenceException class for handling persistence-related errors

---
+++ src/main/java/ude/edu/uy/taller2/exception/AppDataPersistenceException.java
@@ -0,0 +1,7 @@
+package ude.edu.uy.taller2.exception;
+
+public class AppDataPersistenceException extends Exception {
+    public AppDataPersistenceException(String message) {
+        super(message);
+    }
+}
