39ad44bb8fdbf75ed009bb2d4530347044a1b9da
Shoan Genes <shoangenes42@gmail.com>
2026-03-03 08:06:12 -0300

    Add TestDessertListPanel class for testing DessertListPanel functionality

---
+++ src/test/java/ude/edu/uy/taller2/TestDessertListPanel.java
@@ -0,0 +1,25 @@
+package ude.edu.uy.taller2;
+
+import ude.edu.uy.taller2.client.controller.DessertController;
+import ude.edu.uy.taller2.logic.ILogicLayer;
+import ude.edu.uy.taller2.ui.dessert.DessertListPanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.rmi.Naming;
+
+public class TestDessertListPanel {
+    public static void main(String[] args) throws Exception {
+        ILogicLayer logicLayer = (ILogicLayer) Naming.lookup(Config.getURL());
+
+        DessertController dessertController = new DessertController(logicLayer);
+
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new JFrame("Test - Dessert List Panel");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setBounds(100, 100, 600, 400);
+            frame.setContentPane(new DessertListPanel(dessertController));
+            frame.setVisible(true);
+        });
+    }
+}
