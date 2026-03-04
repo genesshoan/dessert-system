858d025683fc43c98ad34706871b2f0137d0065a
Shoan Genes <shoangenes42@gmail.com>
2026-03-04 14:07:27 -0300

    Add BaseController class for managing UI component connections

---
+++ src/main/java/ude/edu/uy/taller2/client/controller/BaseController.java
@@ -0,0 +1,38 @@
+package ude.edu.uy.taller2.client.controller;
+
+import ude.edu.uy.taller2.logic.ILogicLayer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public abstract class BaseController<V extends Component> {
+    protected ILogicLayer logicLayer;
+    protected V view;
+    private boolean isConnected;
+
+    public BaseController(V view) {
+        this.view = view;
+
+        try {
+            logicLayer = RMIConnection.getInstance();
+            isConnected = true;
+        } catch (RemoteException | MalformedURLException | NotBoundException e) {
+            connectionError();
+        }
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+
+    protected void showError(String title, String message) {
+        JOptionPane.showMessageDialog(view, message, title, JOptionPane.ERROR_MESSAGE);
+    }
+
+    protected void connectionError() {
+        showError("Connection error", "Server is not available right now");
+    }
+}
